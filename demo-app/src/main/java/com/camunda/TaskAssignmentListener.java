095aa7d9f47f93b32de282f9b6305989554d970d
KontosPetros <88282132+KontosPetros@users.noreply.github.com>
2024-04-08 17:54:43 +0300

    Add core implementation for java spring boot app

---
+++ demo-app/src/main/java/com/camunda/TaskAssignmentListener.java
@@ -0,0 +1,77 @@
+package com.camunda;
+
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.SimpleEmail;
+import org.camunda.bpm.engine.IdentityService;
+import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.delegate.TaskListener;
+import org.camunda.bpm.engine.identity.User;
+import org.camunda.bpm.engine.impl.context.Context;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TaskAssignmentListener implements TaskListener {
+    protected static final String HOST = "mail-hog";
+    protected static final int PORT = 1025;
+
+    protected final static Logger LOGGER = LoggerFactory.getLogger(TaskAssignmentListener.class);
+
+    public void notify(DelegateTask delegateTask) {
+
+        String assignee = delegateTask.getAssignee();
+        String taskId = delegateTask.getId();
+        LOGGER.info(
+                "assignee '" + assignee);
+        LOGGER.info(
+                "taskId '" + taskId);
+
+        if (assignee != null) {
+
+            IdentityService identityService = Context.getProcessEngineConfiguration().getIdentityService();
+            User user = identityService.createUserQuery().userId(assignee).singleResult();
+
+            LOGGER.info(
+                    "user '" + user);
+
+
+
+            if (user != null) {
+
+                // Get Email Address from User Profile
+                String recipient = user.getEmail();
+
+                if (recipient != null && !recipient.isEmpty()) {
+
+                    Email email = new SimpleEmail();
+                    email.setCharset("utf-8");
+                    email.setHostName(HOST);
+                    email.setSmtpPort(PORT);
+
+                    try {
+                        email.setFrom("noreply@camunda.org");
+                        email.setSubject("Task assigned: " + delegateTask.getName());
+                        email.setMsg("Please set temperature value in task : http://localhost:8080/camunda/app/tasklist/default/#/task=" + taskId);
+
+                        email.addTo(recipient);
+
+                        LOGGER.info(
+                                "Email-------------------------------------- '" + email);
+                        email.send();
+
+                    } catch (Exception e) {
+                        LOGGER.info("Could not send email to assignee", e);
+                    }
+
+                } else {
+                    LOGGER.info("Not sending email to user " + assignee + "', user has no email address.");
+                }
+
+            } else {
+                LOGGER.info("Not sending email to user " + assignee + "', user is not enrolled with identity service.");
+            }
+
+        }
+
+    }
+
+}
--- readme.md
+++ readme.md
@@ -13,3 +13,27 @@ See files [HERE](./camundaForm/)
 ![claim_sreenshoot](./camundaForm/claim.png)
 
 ![quick_demo](./camundaForm/camunda_form.gif)
+
+
+# Java 
+## Introduction
+This is a Java Spring Boot Demo Application as an example of integrating Camunda Forms and TaskListener within a Java Spring Boot framework. It automates a process that triggers an email to the logged-in user, asking them to set a temperature value. The process ends when the user submits the temperature value.
+
+## Getting Started
+
+To run the application:
+
+1. Run`docker-compose up` inside demo-app.
+2. Access the MailHog email server interface at [http://localhost:8025/](http://localhost:8025/).
+3. Open the Camunda interface at [http://localhost:8080/camunda/](http://localhost:8080/camunda/).
+4. Start the "Task Assignment Email" process within Camunda.
+5. Check your email via MailHog .
+6. Set the temperature value.
+
+
+![Process Diagram](demo-app/src/main/resources/process-diagram.png)
+
+![MailHog Example](demo-app/src/main/resources/mail-hog-example.png)
+
+![Form Example](demo-app/src/main/resources/form.png)
+
